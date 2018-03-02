1ae15f173881901dc0ca25eea69237653ee20f3b
SabrinaVillalobos <wafflish@gmail.com>
2018-03-02 00:38:21 -0300

    styles

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import WeatherTemperature from './WeatherTemperature';
 import WeatherExtraInfo from './WeatherExtraInfo';
+import {CLOUD, CLOUDY, SUN, RAIN, SNOW, WINDY} from './../../../constant/weathers';
+import './styles.css';
 
 const WeatherData = () => (
-  <div>
-    <WeatherTemperature temperature={25}/>
+  <div className ='weatherDataCont'>
+    <WeatherTemperature temperature = {23} weatherState = {SNOW}/>
     <WeatherExtraInfo humidity = {80} wind = {'10m/s'}/>
   </div>
-);
+)
+
 export default WeatherData;
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,13 +1,12 @@
-// Primero importamos react
 import React from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
-// Creando primer componente funcional
 const WeatherLocation = () => (
   <div>
     <Location city = {'Santiago'}/>
-    <WeatherData/>
+    <WeatherData />
   </div>
-);
-// Se exporta sin la extensión
+  )
+
+
 export default WeatherLocation;
